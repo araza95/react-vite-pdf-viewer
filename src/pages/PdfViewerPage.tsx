0c8213d47cbf46a19569804491b8d7dbc2263a71
araza95 <alihaiderizvi.you@gmail.com>
2025-03-04 05:44:24 +0100

    feat: Added Search plugin within pdf toolbar

--- src/pages/PdfViewerPage.tsx
+++ src/pages/PdfViewerPage.tsx
@@ -1,19 +1,31 @@
 // React Imports
 import { Fragment, FunctionComponent } from "react";
 
+// Layout Imports
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
 // PDF Imports
 import { SpecialZoomLevel, Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/search/lib/styles/index.css";
 
 // Assets
 import pdf from "../sample.pdf";
+import { OnHighlightKeyword } from "@react-pdf-viewer/search";
 
 const PdfViewer: FunctionComponent = () => {
   // Your render function
   const defaultLayoutPluginInstance = defaultLayoutPlugin({
     toolbarPlugin: {
+      searchPlugin: {
+        keyword: ["example"],
+        onHighlightKeyword: (props: OnHighlightKeyword) => {
+          console.log({ props });
+          props.highlightEle.style.outline = "2px dashed blue";
+          props.highlightEle.style.backgroundColor = "rgba(0, 0, 0, .1)";
+        },
+      },
       fullScreenPlugin: {
         // Zoom to fit the screen after entering and exiting the full screen mode
         onEnterFullScreen: (zoom) => {
